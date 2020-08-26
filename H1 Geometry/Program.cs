2631b3d15c1d2faae71a5dfff922468d2c3e4be8
Marcus Jensen <marcusproman0507@gmail.com>
2020-08-26 11:55:21 +0200

    Add project files.

---
+++ H1 Geometry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace H1_Geometry
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Square squareOne = new Square(2);
+            Square squareTwo = new Square(4);
+            Square squareThree = new Square(8);
+            Square squareFour = new Square(16);
+            Square squareFive = new Square(32);
+
+            Console.WriteLine("This square has a perimeter of {0}", squareThree.GetPerimeter());
+            Console.WriteLine("This square has an area of {0}", squareThree.GetArea());
+
+
+            squareThree.Side = 5;
+            Console.WriteLine("CHANGED");
+
+            Console.WriteLine("This square has a perimeter of {0}", squareThree.GetPerimeter());
+            Console.WriteLine("This square has an area of {0}", squareThree.GetArea());
+
+            Console.ReadKey(true);
+            }
+        }
+    }
---
+++ H1 Geometry/Square.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H1_Geometry
+    {
+    public class Square
+        {
+        #region Fields
+        private double side;
+        #endregion
+
+        #region Properties
+        public double Side
+            {
+            get { return side; }
+            set { side = value; }
+            }
+        #endregion
+
+        #region Constructor
+        public Square()
+            {
+
+            }
+        public Square(double side)
+            {
+            this.side = side;
+            }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Get the perimeter
+        /// </summary>
+        /// <returns>This method returns the perimeter of a square</returns>
+        public double GetPerimeter()
+            {
+            return 4 * side;
+            }
+
+        /// <summary>
+        /// Get the perimeter with x amount of decimals
+        /// </summary>
+        /// <param name="decimals">Specify the amount of decimals to include</param>
+        /// <returns>This method returns the perimeter of a square, with a given amount of decimals</returns>
+        public double GetPerimeter(byte decimals)
+            {
+            return Math.Round(4 * side, decimals, MidpointRounding.AwayFromZero);
+            }
+
+        /// <summary>
+        /// Get the area of the square
+        /// </summary>
+        /// <returns>This method returns the area of the square</returns>
+        public double GetArea()
+            {
+            return side * side;
+            }
+
+        /// <summary>
+        /// Get the area with x amount of decimals
+        /// </summary>
+        /// <param name="decimals">Specify the amount of decimals to include</param>
+        /// <returns>This method returns the area of the square, with a given amount of decimals</returns>
+        public double GetArea(byte decimals)
+            {
+            return Math.Round(side * side, decimals, MidpointRounding.AwayFromZero);
+            }
+        #endregion
+        }
+    }
